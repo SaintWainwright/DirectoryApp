9eca3df2e42b29973813fb5966548d4d6c834e15
cpe3a6a31 <cpe3a6a31@CPE-PC31>
2023-08-22 16:49:43 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MyMAUIApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Shell.Current.Title = "Login Page";
+        }
+
+        private void OnLoginClick(object sender, EventArgs e)
+        {
+            string adminUsername = "admin123";
+            string adminPassword = "password123";
+
+            if((string.IsNullOrEmpty(entryUsername.Text)) || (string.IsNullOrEmpty(entryPassword.Text)))
+            {
+                MessageDisplay.Text = "Username and/or Password should not be empty. Please try again.";
+            }
+
+            else if (adminUsername == entryUsername.Text && adminPassword == entryPassword.Text)
+            {
+                MessageDisplay.Text = "Login Successful";
+            }
+            else
+            {
+                MessageDisplay.Text = "Username and/or Password is incorrect. Please try again";
+            }
+        }
+    }
+}
