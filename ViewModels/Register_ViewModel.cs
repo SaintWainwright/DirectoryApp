eb07b58fefe8a2c0807daf9981799ca33f859028
WAYNESON CLYV SANTOS <waynesonclyv.santos.21@usjr.edu.ph>
2023-10-01 05:36:23 +0800

    Need to make SelectedIndexChanged on School Programs Work.

--- ViewModels/Register_ViewModel.cs
+++ ViewModels/Register_ViewModel.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using DirectoryApp.ContentPages;
 using System.Windows.Input;
 using DirectoryApp.View;
+using System.Collections.ObjectModel;
 
 namespace DirectoryApp.ViewModels
 {
@@ -14,6 +15,7 @@ namespace DirectoryApp.ViewModels
         public Register_ViewModel()
         {
             NewStudent = new Student();
+            InitializeSchoolPrograms();
         }
         private Student _NewStudent;
         public Student NewStudent
@@ -74,6 +76,81 @@ namespace DirectoryApp.ViewModels
                     Application.Current.MainPage.DisplayAlert("Mobile No", "Number Input Only", "Okay");
                 }
             }
+        }
+
+        private ObservableCollection<String> _SchoolProgram;
+        private ObservableCollection<String> _Courses;
+        private ObservableCollection<String> _YearLevel;
+
+        public ObservableCollection<String> SchoolProgram
+        {
+            get { return  _SchoolProgram; }
+            set
+            {
+                _SchoolProgram = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(_SchoolProgram));
+            }
+        }
+
+        public ObservableCollection<String> Courses
+        {
+            get { return _Courses; }
+            set
+            {
+                _Courses = value;
+                OnPropertyChanged(nameof(_Courses));
+            }
+        }
+
+        public ObservableCollection<String> YearLevel
+        {
+            get { return _YearLevel; }
+            set
+            {
+                _YearLevel = value;
+                OnPropertyChanged(nameof(_YearLevel));
+            }
+        }
+
+        private void InitializeSchoolPrograms()
+        {
+            SchoolProgram = new ObservableCollection<string>
+            {
+                "-SELECT-",
+                "School of Engineering",
+                "School of Computer Science",
+                "School of Arts and Sciences",
+                "School of Business and Management",
+                "School of Allied Medical Service",
+                "School of Education",
+                "School of Law",
+            };
+        }
+        public void InitializeCourses()
+        {
+            Courses = new ObservableCollection<string>();
+            if(SchoolProgramIndex == 1)
+            {
+                Courses.Add("Computer Engineering");
+                Courses.Add("Civil Engineering");
+                Courses.Add("Industrial Engineering");
+                Courses.Add("Electrical Engineering");
+                Courses.Add("Electronics Engineering");
+                Courses.Add("Mechanical Engineering");
+            }
+            else if(SchoolProgramIndex == 2)
+            {
+                Courses.Add("Information Technology");
+                Courses.Add("Computer Sciences");
+                Courses.Add("Information Systems");
+            }
+        }
+        public int _SchoolProgramIndex = 0;
+        public int SchoolProgramIndex
+        {
+            get { return _SchoolProgramIndex; }
+            set { _SchoolProgramIndex = value; OnPropertyChanged(); OnPropertyChanged(nameof(_SchoolProgramIndex));}
         }
     }
 }
